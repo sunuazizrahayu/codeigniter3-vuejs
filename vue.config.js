0e10870da7e26f70a726891400d985a538ca95c4
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2021-03-06 11:44:44 +0700

    add vuejs with vue-cli

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/production-sub-path/'
+  //   : '/',
+  outputDir: path.resolve(__dirname, "public/dist"),
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'client')
+      }
+    },
+    entry: {
+      app: path.join(__dirname, 'client', 'main.js')
+    }
+  }
+}
