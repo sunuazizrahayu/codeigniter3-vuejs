a9961476681a7f7d4071318c45db3cdeb70a7dd5
Sunu Aziz Rahayu <sunuazizrahayu@gmail.com>
2021-03-10 07:42:33 +0700

    add docker-compose for development purpose

---
+++ docker/Dockerfile.vue
@@ -0,0 +1,13 @@
+FROM node:lts-alpine3.13
+
+# NodeJS
+WORKDIR /app
+COPY package*.json /app/
+RUN npm install
+
+COPY ./client /app/client
+COPY ./babel.config.js /app/.
+COPY ./vue.config.js /app/.
+
+EXPOSE 8080
+CMD ["npm", "run", "serve"]
